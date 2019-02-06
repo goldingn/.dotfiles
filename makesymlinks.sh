1d46268b0dd05fbc7179b1a658fe69782e827c36
Nick Golding <goldingn35@gmail.com>
2019-02-07 09:21:32 +1100

    add zsh files to bash script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="hyper.js profile Rprofile gitignore gitconfig vimrc vim scripts git-completion.bash"    # list of files/folders to symlink in homedir
+files="hyper.js profile Rprofile gitignore gitconfig vimrc vim scripts git-completion.bash zshrc zprofile"    # list of files/folders to symlink in homedir
 
 ##########
 
